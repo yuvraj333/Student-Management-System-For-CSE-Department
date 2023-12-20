18c3b18a3fe20e41489f843598cce938650207f2
Yuvraj Kumar Mahato <70275777+yuvraj333@users.noreply.github.com>
2023-12-20 15:08:39 +0545

    first commit

---
+++ student_management_app/views.py
@@ -0,0 +1,64 @@
+# from channels.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from student_management_app.EmailBackEnd import EmailBackEnd
+
+def index1(request):
+    return render(request,'index1.html')
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def loginPage(request):
+    return render(request, 'login.html')
+
+
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
+            if user_type == '1':
+                return redirect('admin_home')
+                
+            elif user_type == '2':
+                # return HttpResponse("Staff Login")
+                return redirect('staff_home')
+                
+            elif user_type == '3':
+                # return HttpResponse("Student Login")
+                return redirect('student_home')
+            else:
+                messages.error(request, "Invalid Login!")
+                return redirect('index1')
+        else:
+            messages.error(request, "Invalid Login Credentials!")
+            #return HttpResponseRedirect("/")
+            return redirect('index1')
+
+
+
+def get_user_details(request):
+    if request.user != None:
+        return HttpResponse("User: "+request.user.email+" User Type: "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
